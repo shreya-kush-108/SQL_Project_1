2fdbdbdc3ba8315a24c0543d8bdbe401c9c85565
Shreya Kushwaha <shreyakush108@gmail.com>
2024-07-13 13:39:46 +0530

    Advanced_Level_2_Queries.sql

--- Advanced_Level_2_Queries.sql
+++ Advanced_Level_2_Queries.sql
@@ -10,9 +10,9 @@ ORDER BY price DESC LIMIT 5;
 -- Q42. Average Review Score by Product Category using a CTE
 
 WITH review_scores AS (SELECT p.product_category_name, r.review_score
-						FROM products AS p
-						JOIN order_items i ON p.product_id = i.product_id
-						JOIN order_review r ON i.order_id = r.order_id)
+			FROM products AS p
+			JOIN order_items i ON p.product_id = i.product_id
+			JOIN order_review r ON i.order_id = r.order_id)
                         
 SELECT product_category_name, AVG(review_score) AS avg_review_score
 FROM review_scores
@@ -22,8 +22,8 @@ GROUP BY product_category_name;
 -- Q43. Orders and Payments Summary using a CTE
 
 WITH orders_summary AS (SELECT o.order_id, o.order_status, p.payment_value
-						FROM orders AS o
-						JOIN order_payment AS p ON o.order_id = p.order_id)
+			FROM orders AS o
+			JOIN order_payment AS p ON o.order_id = p.order_id)
 
 SELECT order_status, COUNT(*) AS total_orders, SUM(payment_value) AS total_revenue
 FROM orders_summary
@@ -34,12 +34,12 @@ GROUP BY order_status;
 
 WITH 
 	customer_spending AS (SELECT customer_id, SUM(payment_value) AS total_spent
-						   FROM order_payment AS p
-						   JOIN orders AS o ON p.order_id = o.order_id
-						   GROUP BY customer_id),
+				FROM order_payment AS p
+				JOIN orders AS o ON p.order_id = o.order_id
+	       		        GROUP BY customer_id),
 
 	avg_spending AS (SELECT AVG(total_spent) AS avg_spent
-							  FROM customer_spending)
+			FROM customer_spending)
                  
 SELECT cs.customer_id, cs.total_spent
 FROM customer_spending AS cs
@@ -50,9 +50,9 @@ WHERE cs.total_spent > avg_spent;
 -- Q45. Monthly Revenue Growth using a CTE
 
 WITH monthly_revenue AS (SELECT DATE_FORMAT(order_purchase_timestamp, '%Y-%m') AS month, SUM(payment_value) AS total_revenue
-						 FROM orders AS o
-						 JOIN order_payment AS p ON o.order_id = p.order_id
-						 GROUP BY month)
+			 FROM orders AS o
+			 JOIN order_payment AS p ON o.order_id = p.order_id
+			 GROUP BY month)
                          
 SELECT month, total_revenue, 
 		(total_revenue - LAG(total_revenue) OVER (ORDER BY month)) / LAG(total_revenue) OVER (ORDER BY month) * 100 AS growth_rate
